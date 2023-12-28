4a3e21773517953b46473672f684eee419d6709e
lucylima <lucyatmosphere@gmail.com>
2023-12-28 17:32:47 -0300

    adição de imagem e implementação dos botôes

--- src/Components/Apresentacao.tsx
+++ src/Components/Apresentacao.tsx
@@ -7,9 +7,12 @@ const Primeirapagina = () => {
       <div className="header">
         <h1>A maneira mais fácil de gerenciar projetos</h1>
         <p>Desde as pequenas coisas até a visão geral, organiza o trabalho para que as equipes saibam o que fazer, por que é importante e como concluí-lo.</p>
+      <button>Começar</button>
+      <button><img src="" alt="" />Assistir ao vídeo</button>
       </div>
-      <div>
-        <img src="src/Assets/Mobile.png" alt="" id="mockup1"/>
+   
+      <div className="phone">
+        <img src="src/Assets/Mobile.png" alt="" />
       </div>
     </div>
     
